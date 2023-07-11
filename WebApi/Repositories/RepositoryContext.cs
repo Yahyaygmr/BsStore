f21ac1755fbb45f66858cff1d6b7742cd0ab5e51
Yahya <YAHYA@DESKTOP-4QIIH5S>
2023-07-11 22:48:07 +0300

    Add project files.

---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+           
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
